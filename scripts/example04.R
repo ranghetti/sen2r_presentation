9c825bb0c6e2ac7f7d57e9994d67b49dfb2e00d0
Luigi Ranghetti <ggranga@gmail.com>
2018-07-17 15:21:27 +0200

    Update

--- scripts/example04.R
+++ scripts/example04.R
@@ -28,31 +28,40 @@ msavi_aggr <- sprawl::extract_rast(
 )
 
 # Plot time series for some fields
-id_sample <- as.factor(c(
-  912, 933, 945, 937, # mais (di prima e seconda)
+sample_coltures <- data.frame(
+  "id" = as.factor(c(
+    912, 933, 945, 937, # mais (di prima e seconda)
     5, 306, 986,  32, # riso (allagato e in asciutta)
-  882, 849, 859, 883  # riso (in asciutta con sovescio)
-))
+    882, 849, 859, 883  # riso (in asciutta con sovescio)
+  )),
+  "colture" = c(
+    "Maize", rep("Winter crop + maize", 3),
+    rep("Rice (water seeding)", 2), rep("Rice (dry seeding)", 2), 
+    rep("Rice (green manure\n+ dry seeding)", 4)
+  ),
+  stringsAsFactors = FALSE
+)
 msavi_aggr_sample <- msavi_aggr$stats[
-  msavi_aggr$stats$id %in% id_sample &
+  msavi_aggr$stats$id %in% sample_coltures$id &
     !is.na(msavi_aggr$stats$med),]
-msavi_aggr_sample$id <- factor(msavi_aggr_sample$id, levels = id_sample)
+msavi_aggr_sample$id <- factor(msavi_aggr_sample$id, levels = sample_coltures$id)
 
 # Compute additional information
 msavi_aggr_meta <- fs2nc_getElements(msavi_aggr_sample$band_name, format = "data.frame")
 msavi_aggr_sample$date <- msavi_aggr_meta$sensing_date
 msavi_aggr_sample$sensor <- msavi_aggr_meta$mission # Sentinel-2A or 2B
 msavi_aggr_sample$orbit <- msavi_aggr_meta$id_orbit # Sentinel-2 orbit
-msavi_aggr_sample$med <- msavi_aggr_sample$med / 1E4 # from -1000:1000 to -1:1
+msavi_aggr_sample$MSAVI <- msavi_aggr_sample$med / 1E4 # from -1000:1000 to -1:1
+msavi_aggr_sample$colture <- sample_coltures[match(msavi_aggr_sample$id, sample_coltures$id), "colture"]
 
 # Build plot of time series
 Sys.setlocale("LC_TIME", "en_GB.UTF-8")
-msavi_plot_ts <- ggplot(msavi_aggr_sample, aes(x = date, y = med)) +
+msavi_plot_ts <- ggplot(msavi_aggr_sample, aes(x = date, y = MSAVI, label = colture)) +
   geom_line() + 
   geom_point(aes(colour = orbit, shape = sensor), size = 2) +
+  # geom_text(data = sample_coltures, x = as.Date("2018-05-15"), y = 0, aes(label = colture)) +
   facet_wrap(~id) +
   coord_cartesian(ylim = c(0, 1)) +
-  xlab("Date") + ylab("MSAVI") + 
   theme_light()
 msavi_plotly <- ggplotly(msavi_plot_ts)
 
